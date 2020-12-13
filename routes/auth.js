9b3a8627a1c83a27c8552665311f96071a7acb3e
Dalton Heck <daltonheck1993@gmail.com>
2020-12-12 23:39:38 -0500

    make it better

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken")
-const {JWT_SECRET} = require("../keys")
+const {JWT_SECRET} = require("../config/keys")
 const requireLogin = require("../middleware/requireLogin")
 
 
